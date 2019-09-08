80a1ddc675edebf399829c62442e0fe4bb8014d2
kusamochi0706 <hayatsu.h.0706@gmail.com>
2019-09-08 22:33:22 +0900

    GPS機能を搭載する前に、現在の状態をコミット。

--- app/src/main/java/com/example/slotgame/MainActivity.java
+++ app/src/main/java/com/example/slotgame/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.slotgame;
 
-import android.inputmethodservice.KeyboardView;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -11,31 +10,35 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    int droidSide1 = -1;
-    int droidSide2 = -1;
-    int droidSide3 = -1;
+    int slotnum1 = -1;
+    int slotnum2 = -1;
+    int slotnum3 = -1;
     Button retryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final ImageView droidImage1 = (ImageView) this.findViewById(R.id.droidimageid1);
-        final ImageView droidImage2 = (ImageView) this.findViewById(R.id.droidimageid2);
-        final ImageView droidImage3 = (ImageView) this.findViewById(R.id.droidimageid3);
-        final Random r = new Random();
+        final ImageView slotImage1 = (ImageView) this.findViewById(R.id.slotimageid1);
+        final ImageView slotImage2 = (ImageView) this.findViewById(R.id.slotimageid2);
+        final ImageView slotImage3 = (ImageView) this.findViewById(R.id.slotimageid3);
+
         final Button b1 = (Button) this.findViewById(R.id.slotbutton1);
         final Button b2 = (Button) this.findViewById(R.id.slotbutton2);
         final Button b3 = (Button) this.findViewById(R.id.slotbutton3);
+
         retryButton = (Button) this.findViewById(R.id.retrybutton);
+
         retryButton.setVisibility(View.INVISIBLE);
+
+        final Random r = new Random();
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                droidSide1 = r.nextInt(3);
-                droidImage1.setImageResource(getDrawableId(droidSide1));
+                slotnum1 = r.nextInt(3);
+                slotImage1.setImageResource(getDrawableId(slotnum1));
                 checkSlot();
                 b1.setEnabled(false);
             }
@@ -44,8 +47,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                droidSide2 = r.nextInt(3);
-                droidImage2.setImageResource(getDrawableId(droidSide2));
+                slotnum2 = r.nextInt(3);
+                slotImage2.setImageResource(getDrawableId(slotnum2));
                 checkSlot();
                 b2.setEnabled(false);
             }
@@ -54,8 +57,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                droidSide3 = r.nextInt(3);
-                droidImage3.setImageResource(getDrawableId(droidSide3));
+                slotnum3 = r.nextInt(3);
+                slotImage3.setImageResource(getDrawableId(slotnum3));
                 checkSlot();
                 b3.setEnabled(false);
             }
@@ -63,36 +66,36 @@ public class MainActivity extends AppCompatActivity {
         retryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                droidImage1.setImageResource(R.drawable.start1);
-                droidImage2.setImageResource(R.drawable.start1);
-                droidImage3.setImageResource(R.drawable.start1);
+                slotImage1.setImageResource(R.drawable.start1);
+                slotImage2.setImageResource(R.drawable.start1);
+                slotImage3.setImageResource(R.drawable.start1);
                 b1.setEnabled(true);
                 b2.setEnabled(true);
                 b3.setEnabled(true);
                 retryButton.setVisibility(View.INVISIBLE);
-                droidSide1 = droidSide2 = droidSide3 = -1;
+                slotnum1 = slotnum2 = slotnum3 = -1;
             }
         });
     }
 
-    private int getDrawableId(int side) {
+    private int getDrawableId(int num) {
         int drawableId = -2;
-        if (side == 0) {
+        if (num == 0) {
             drawableId = R.drawable.chuka;
-        } else if (side == 1) {
+        } else if (num == 1) {
             drawableId = R.drawable.pakutii;
-        } else if (side == 2) {
+        } else if (num == 2) {
             drawableId = R.drawable.itarian;
         }
         return drawableId;
     }
 
     private void checkSlot() {
-        if (droidSide1 == droidSide2 && droidSide1 == droidSide3) {
+        if (slotnum1 == slotnum2 && slotnum1 == slotnum3) {
             Toast t = Toast.makeText(getApplicationContext(), "おめでとう！", Toast.LENGTH_SHORT);
             t.show();
             retryButton.setVisibility(View.VISIBLE);
-        } else if (droidSide1 != -1 && droidSide2 != -1 && droidSide3 != -1) {
+        } else if (slotnum1 != -1 && slotnum2 != -1 && slotnum3 != -1) {
             retryButton.setVisibility(View.VISIBLE);
         }
     }
