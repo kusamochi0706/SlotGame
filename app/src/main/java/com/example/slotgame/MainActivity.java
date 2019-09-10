5f956147666a223c9cd5182f468f18f74ea322ee
kusamochi0706 <hayatsu.h.0706@gmail.com>
2019-09-10 20:52:41 +0900

    GPS取得ボタンとGPS取得ストップボタンを配置 GPS取得メソッド実装

--- app/src/main/java/com/example/slotgame/MainActivity.java
+++ app/src/main/java/com/example/slotgame/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.slotgame;
 
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -15,10 +17,59 @@ public class MainActivity extends AppCompatActivity {
     int slotnum3 = -1;
     Button retryButton;
 
+    // GPS用
+    static private android.location.LocationManager locationManager=null;
+    static private LocationListener locationListener=null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(null==locationListener) {
+            locationManager = (android.location.LocationManager) getSystemService(android.content.Context.LOCATION_SERVICE);
+            locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+            };
+        }
+        //位置情報取得開始ボタン
+        ((android.widget.Button) findViewById(R.id.locationButton_1))
+                .setOnClickListener(new android.view.View.OnClickListener() {
+                    public void onClick(android.view.View view) {
+                        if(locationManager.isProviderEnabled(android.location.LocationManager.GPS_PROVIDER)){
+                            //requestLocationUpdates(プロバイダー,通知の最小時間間隔(ms),通知の最小距離間隔(m),リスナー);
+                            locationManager.requestLocationUpdates(android.location.LocationManager.GPS_PROVIDER,0,10,locationListener);
+                        }
+                    }
+                });
+        //位置情報取得停止ボタン
+        ((android.widget.Button) findViewById(R.id.locationButton_2))
+                .setOnClickListener(new android.view.View.OnClickListener() {
+                    public void onClick(android.view.View view) {
+                        locationManager.removeUpdates(locationListener);
+                    }
+                });
+
+
         final ImageView slotImage1 = (ImageView) this.findViewById(R.id.slotimageid1);
         final ImageView slotImage2 = (ImageView) this.findViewById(R.id.slotimageid2);
         final ImageView slotImage3 = (ImageView) this.findViewById(R.id.slotimageid3);
@@ -76,6 +127,23 @@ public class MainActivity extends AppCompatActivity {
                 slotnum1 = slotnum2 = slotnum3 = -1;
             }
         });
+    }
+
+    class _LocationListener implements android.location.LocationListener {
+        @Override
+        public void onLocationChanged(android.location.Location location) {
+            //位置情報を文字列に変換
+            String str=location.getLatitude() + " " + location.getLongitude();
+            android.widget.TextView t=(android.widget.TextView)findViewById(R.id.locationTextView);
+            t.setText(str);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {}
+        @Override
+        public void onProviderEnabled(String provider) {}
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
     }
 
     private int getDrawableId(int num) {
